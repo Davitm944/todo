a54a0cad9c8b1cab077b48579efd41da4ccf99d8
Davit Ter-Mkrtchyan <davitm944@gmail.com>
2022-03-31 08:16:48 +0400

    Adding timer option

--- src/App.js
+++ src/App.js
@@ -3,14 +3,19 @@ import { v4 as uuidv4 } from 'uuid';
 import check from 'components/Icons/check.png';
 import edit from 'components/Icons/pen.png';
 import remove from 'components/Icons/x.png';
+import watch from 'components/Icons/stopwatch.png'
 import EditModal from 'components/EditModal';
 import {DragDropContext, Droppable, Draggable} from 'react-beautiful-dnd'
+import Countdown from 'react-countdown';
+import CountDownModal from 'components/countdown/CountDownModal';
 
 function App() {
 
 	const [ list, setList ] = useState([]);
 	const [ addItem, setAddItem ] = useState('');
 	const [ open, setOpen ] = useState(false);
+	const [ openCount, setOpenCount ] = useState(false);
+	const [time, setTime] = useState(0)
 	const [key, setKey] = useState('')
 
 	const onAdd = () => {
@@ -18,14 +23,13 @@ function App() {
 		const newItem = {
 			value : addItem,
 			key   : uuidv4(),
-			done  : false
+			done  : false,
+			time	: null
 		}
 		newList.push(newItem);
 		setList(newList);
 		setAddItem('');
 	};
-
-
 
 	const onDelete = (key) => {
 		const newList = list.filter((item) => {
@@ -49,6 +53,10 @@ function App() {
 		setList(newList);
 	}
 
+	const handleTimer = (key) => {
+		setKey(key)
+		setOpenCount(true)
+	}
 
 	const onDragEnd = (result) => {
 		if(!result.destination){
@@ -60,13 +68,10 @@ function App() {
 		setList(newList)
 	} 
 
-
-
-
-                   
 	return (
 		<div className='bg-neutral-200 w-screen h-screen flex flex-col'>
 			{open ? <EditModal open={open} setOpen={setOpen} list={list} setList={setList} itemKey={key}  /> : <></>}
+			{openCount ? <CountDownModal setTime={setTime} setOpenCount={setOpenCount} list={list} setList={setList} itemKey={key}/> : <></>}
 			<div className='mx-auto my-5 w-1/2 h-full bg-neutral-400 shadow-lg rounded-sm shadow-gray-500  flex flex-col '>
 				<div className='mx-auto my-12 mt-12 '>
 					<input
@@ -90,14 +95,18 @@ function App() {
 							{(provided ) =>  (
 					<section {...provided.droppableProps} ref={provided.innerRef}>
 						{list.map((item, index) => {
+							console.log(item)
 							return (
 								<Draggable key={item.key} draggableId={item.key} index={index}>
 									{(provided) => ( 
-								<div {...provided.draggableProps} {...provided.dragHandleProps} ref={provided.innerRef } className='flex justify-between px-2 bg-gray-300 border-b-2 border-black'>
+								<div {...provided.draggableProps} {...provided.dragHandleProps} ref={provided.innerRef } className='todo_item'>
 									<div className='max-w-xl w-full'>
 										{item.done ? <span className='line-through'>{item.value}</span> :  <span>{item.value}</span>}
 									</div>
 									<div className='flex'>
+										<div className='wrapper'>
+											<img src={watch} alt='logo' onClick={() => handleTimer(item.key)}/>
+										</div>
 										<div className='wrapper'>
 											<img src={check} alt='logo' onClick={() => onDone(item.key)}/>
 										</div>
@@ -108,6 +117,7 @@ function App() {
 											<img src={remove} alt='logo' onClick={() => onDelete(item.key)} />
 										</div>
 									</div>
+									{item.time ? <div className='timer'><Countdown date={Date.now() + 1000 * 60 * item.time} /></div> : <></>}
 								</div>
 								)}
 								</Draggable>
